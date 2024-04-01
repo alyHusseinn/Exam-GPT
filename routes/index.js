41498af03f500749ba63084f604f12cde9dbdb4d
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-04-01 03:03:34 +0200

    update the routes and adding the auth middlewares to it

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,32 @@
-var express = require('express');
-var router = express.Router();
-// we should have three routes student and teacher and exam
+const examControllers = require("../controllers/exam.controllers");
+const teacherControllers = require("../controllers/teacher.controllers");
+const authControllers = require("../controllers/auth.controllers");
+const checkJWT = require("../middlewares/checkJWT");
+const checkRole = require("../middlewares/checkRole");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.redirect('/home');
+router.get("/", function (req, res, next) {
+  res.redirect("/home");
 });
+/* GET home page. */
+router.get("/home", checkJWT, teacherControllers.getAllTeachers);
+router.get("/teacher/:id", checkJWT, teacherControllers.getTeacher);
+
+// Exam routes
+router.get("/exam/:id", examControllers.exam_get);
+router.post(
+  "/exam",
+  checkJWT,
+  checkRole("teacher"),
+  examControllers.exam_create
+);
+router.post("/exam/id", checkJWT, checkRole("student"), examControllers.exam_submit);
+
+// Auth routesd
+router.get("/signup", authControllers.signup_get);
+router.post("/signup", authControllers.signup_post);
+router.get("/login", authControllers.login_get);
+router.post("/login", authControllers.login_post);
+
+module.exports = router;
 
 module.exports = router;
