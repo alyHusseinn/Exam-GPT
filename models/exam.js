7440690bfae937eac14c1032e43c60d00abbe6fd
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-04-03 16:24:12 +0200

    update the user and exam models

--- models/exam.js
+++ models/exam.js
@@ -1,63 +1,75 @@
-const mongoose = require('mongoose');
-const generateQuestions = require('../Ai/openai');
+const mongoose = require("mongoose");
+const generateQuestions = require("../Ai/openai");
 
 const Schema = mongoose.Schema;
 
 const examSchema = new Schema({
-    topic: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        enum: ['essay', 'mcq'],
-        required: true
-    },
-    numberOfQuestions: {
+  topic: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["essay", "mcq"],
+    required: true,
+  },
+  numberOfQuestions: {
+    type: Number,
+    required: true,
+  },
+  teacher: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  students: [
+    {
+      student: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      score: {
         type: Number,
-        required: true
+      },
     },
-    teacher: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+  ],
+  essayQuestions: [
+    {
+      question: {
+        type: String,
+      },
+      answer: {
+        type: String,
+      },
     },
-    students: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-    }],
-    essayQuestions: [{
-        question: {
-            type: String,
+  ],
+  mcqQuestions: [
+    {
+      question: {
+        type: String,
+      },
+      choices: [
+        {
+          type: String,
         },
-        answer: {
-            type: String
-        }
-    }],
-    mcqQuestions: [{
-        question: {
-            type: String
-        },
-        choices: [{
-            type: String
-        }],
-        answer: {
-            type: Number
-        }
-    }]
+      ],
+      answer: {
+        type: Number,
+      },
+    },
+  ],
 });
 
-examSchema.get('url', function() {
-    return `/exams/${this._id}`;
+examSchema.virtual("url").get(function () {
+  return `/exams/${this._id}`;
 });
 
 // generate the exam questions before saving.
-examSchema.pre('save', async function() {
-    if (this.type === 'mcq') {
-        this.mcqQuestions = await generateQuestions(this.topic, this.numberOfQuestions, true);
-    } else {
-        this.essayQuestions = await generateQuestions(this.topic, this.numberOfQuestions, false);
-    }
-})
+examSchema.pre("save", async function () {
+  this.type === "mcq"
+    ? (this.mcqQuestions = await generateQuestions( this.topic, this.numberOfQuestions, true ))
+    : (this.essayQuestions = await generateQuestions( this.topic, this.numberOfQuestions, false));
+});
 
-module.exports = mongoose.model('Exam', examSchema);
+module.exports = mongoose.model("Exam", examSchema);
--- models/user.js
+++ models/user.js
@@ -1,48 +1,46 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true,
-        enum: ['teacher', 'student'],
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["teacher", "student"],
+  },
 });
 
-userSchema.get('url', function() {
-    if(this.role === 'teacher') {
-        return `/teachers/${this._id}`;
-    } else {
-        return `/students/${this._id}`;
-    }
+userSchema.virtual("url").get(function () {
+  return this.role === "teacher"
+    ? `/teachers/${this._id}`
+    : `/students/${this._id}`;
 });
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.pre('save', async function(next) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
-userSchema.post('save', async function(next) {
-    console.log(this);
+userSchema.post("save", async function (next) {
+  console.log(this);
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
