641205c90517602088946aebad5a9949ba2a4286
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-04-04 18:13:37 +0200

    update the routes

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,38 @@
-const examControllers = require("../controllers/exam.controllers");
-const teacherControllers = require("../controllers/teacher.controllers");
-const authControllers = require("../controllers/auth.controllers");
-const checkJWT = require("../middlewares/checkJwt");
-const checkRole = require("../middlewares/checkRole");
-const router = require('express').Router();
+const examControllers = require('../controllers/exam.controllers')
+const teacherControllers = require('../controllers/teacher.controllers')
+const authControllers = require('../controllers/auth.controllers')
+const checkJWT = require('../middlewares/checkJwt')
+const checkRole = require('../middlewares/checkRole')
+const router = require('express').Router()
 
-router.get("/", function (req, res, next) {
-  res.redirect("/home");
-});
+router.get('/', function (req, res, next) {
+  res.redirect('/home')
+})
 /* GET home page. */
-router.get("/home", checkJWT, teacherControllers.getHomePage);
-router.get("/teacher/:id", checkJWT, teacherControllers.getTeacher);
+router.get('/home', checkJWT, teacherControllers.getHomePage)
+router.get('/teacher/:id', checkJWT, teacherControllers.getTeacher)
 
 // Exam routes
-router.get("/exam/:id", examControllers.exam_get);
+router.get('/exam/:id', checkJWT, examControllers.exam_get)
+
+// post a new exam
 router.post(
-  "/exam",
-  [checkJWT,
-  checkRole("teacher")],
+  '/exam',
+  [checkJWT, checkRole('teacher')],
   examControllers.exam_create
-);
-router.post("/exam/:id", [checkJWT, checkRole("student")], examControllers.exam_submit);
+)
+// submit an exam
+router.post(
+  '/exam/:id',
+  [checkJWT, checkRole('student')],
+  examControllers.exam_submit
+)
 
 // Auth routesd
-router.get("/signup", authControllers.signup_get);
-router.post("/signup", authControllers.signup_post);
-router.get("/login", authControllers.login_get);
-router.post("/login", authControllers.login_post);
-router.post('/logout', authControllers.logout_post);
+router.get('/signup', authControllers.signup_get)
+router.post('/signup', authControllers.signup_post)
+router.get('/login', authControllers.login_get)
+router.post('/login', authControllers.login_post)
+router.post('/logout', authControllers.logout_post)
 
-module.exports = router;
+module.exports = router
