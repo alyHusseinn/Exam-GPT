8990d8dde8f0c4149c2604d251a2a0f883b18f2b
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-04-05 11:48:54 +0200

    adding teacher-profile page and update the controllers

--- controllers/teacher.controllers.js
+++ controllers/teacher.controllers.js
@@ -1,24 +1,37 @@
-const Teacher = require('../models/user');
-const asyncHandler = require('express-async-handler');
+const Teacher = require('../models/user')
+const Exam = require('../models/exam')
+const asyncHandler = require('express-async-handler')
 /* const { body, validationResult } = require('express-validator'); */
 
 exports.getHomePage = asyncHandler(async (req, res) => {
-    const teachers = await Teacher.find({role: 'teacher'}).select('-password').exec();
-    res.render('home', {
-        title: 'home',
-        teachers: teachers
-    })
-});
+  const teachers = await Teacher.find({ role: 'teacher' })
+    .select('-password')
+    .exec()
+  res.render('home', {
+    title: 'home',
+    teachers: teachers
+  })
+})
+// fib function
 
 exports.getTeacher = asyncHandler(async (req, res) => {
-    const teacher = await Teacher.findById(req.params.id);
-    if(!teacher) {
-        const error = new Error('Teacher not found');
-        error.status = 404;
-        throw error;
-    }
-    res.render('teacher', {
-        title: `${teacher.username}'s profile`,
-        teacher: teacher
-    });
+  const [teacher, exams] = await Promise.all([
+    Teacher.findById(req.params.id).select('-password').exec(),
+    Exam.find({ teacher: req.params.id })
+      .select('topic numberOfQuestions type')
+      .exec()
+  ])
+
+  // if teacher doesn't exist in database return 404
+
+  if (!teacher) {
+    const error = new Error('Teacher not found')
+    error.status = 404
+    throw error
+  }
+  res.render('teacher_profile', {
+    title: `${teacher.username}'s profile`,
+    teacher,
+    exams
+  })
 })
