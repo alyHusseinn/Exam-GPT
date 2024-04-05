2da565f8bb8936f3485862832fe4159286d22300
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-04-05 15:25:23 +0200

    package.json and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-      main: '#cbcdc2',
-      secondary: '#d9d9d9',
-      text: 'black',
-      green: '#0c6004',
-      red: '#ff0000',
-      blue: '#4f96e9'
-    },
-  }},
-  plugins: []
+        main: '#cbcdc2',
+        secondary: '#d9d9d9',
+        text: 'black',
+        green: '#0c6004',
+        red: '#ff0000',
+        blue: '#4f96e9'
+      }
+    }
+  },
+  // add hide scrollbar utilitey
+  plugins: [
+    // function ({ addUtilites }) {
+    //   const newUtitley = {
+    //     '.no-scrollbar::webkit-scrollbar': {
+    //       display: 'none'
+    //     },
+    //     '.no-scrollbars': {
+    //       display: 'none',
+    //       'scrollbar-width': 'none',
+    //       '-ms-overflow-width': 'none'
+    //     }
+    //   }
+    //   addUtilites(newUtitley);
+    // }
+  ]
 }
