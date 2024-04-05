e44ef40d044dedbcc9c56762dabfbdfd66c361b1
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-04-05 11:49:31 +0200

    adding custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./public/*.{html,js,css}",
-    "./views/*.ejs",
-  ],
+  content: ['./public/*.{html,js,css}', './views/*.ejs'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+      main: '#cbcdc2',
+      secondary: '#d9d9d9',
+      text: 'black',
+      green: '#0c6004',
+      red: '#ff0000',
+      blue: '#4f96e9'
+    },
+  }},
+  plugins: []
 }
