38d8a4ec6df8713656475a509326c00bef63aaec
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-04-05 15:24:22 +0200

    update controllers

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,129 +1,129 @@
-const asyncHandler = require("express-async-handler");
-const User = require("../models/user");
-const { body, validationResult } = require("express-validator");
-const multer = require("multer");
-const { storage } = require("../storage");
-const jwt = require("jsonwebtoken");
-const { NODE_ENV, JWT_SECRET } = require("../config/env");
+const asyncHandler = require('express-async-handler')
+const User = require('../models/user')
+const { body, validationResult } = require('express-validator')
+const multer = require('multer')
+const { storage } = require('../storage')
+const jwt = require('jsonwebtoken')
+const { NODE_ENV, JWT_SECRET } = require('../config/env')
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
 exports.signup_get = (req, res) => {
-  res.render("signup", {
-    title: "Register",
-  });
-};
+  res.render('signup', {
+    title: 'Register'
+  })
+}
 
 exports.signup_post = [
-  upload.single("avatar"),
-  body("username", "Please enter a valid username")
+  upload.single('avatar'),
+  body('username', 'Please enter a valid username')
     .trim()
     .escape()
     .isLength({ min: 5 })
-    .withMessage("Username must be at least 5 characters long"),
-  body("password", "Please enter a valid password")
+    .withMessage('Username must be at least 5 characters long'),
+  body('password', 'Please enter a valid password')
     .trim()
     .escape()
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long"),
+    .withMessage('Password must be at least 8 characters long'),
   asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      res.render("signup", {
-        title: "Register",
-        errors: errors.array(),
-      });
+      res.render('signup', {
+        title: 'Register',
+        errors: errors.array()
+      })
     } else {
-      const { username, password, role } = req.body;
+      const { username, password, role } = req.body
       const user = new User({
         username,
         password,
         avatar: req.file.path,
-        role,
-      });
+        role
+      })
       try {
-        await user.save();
-        res.redirect("/login");
+        await user.save()
+        res.redirect('/login')
       } catch (err) {
-        console.log(err);
-        res.render("signup", {
-          title: "Register",
+        console.log(err)
+        res.render('signup', {
+          title: 'Register',
           errors: [
             {
-              msg: err.message,
-            },
-          ],
-        });
+              msg: err.message
+            }
+          ]
+        })
       }
     }
-  }),
-];
+  })
+]
 
 exports.login_get = (req, res) => {
-  res.render("login", {
-    title: "Login",
-  });
-};
+  res.render('login', {
+    title: 'Login'
+  })
+}
 
 exports.login_post = [
-  body("username", "Please enter a valid username")
+  body('username', 'Please enter a valid username')
     .trim()
     .escape()
     .isLength({ min: 5 })
-    .withMessage("Username must be at least 5 characters long"),
-  body("password", "Please enter a valid password")
+    .withMessage('Username must be at least 5 characters long'),
+  body('password', 'Please enter a valid password')
     .trim()
     .escape()
     .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long"),
+    .withMessage('Password must be at least 8 characters long'),
 
   asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      res.render("login", {
-        title: "Login",
-        errors: errors.array(),
-      });
+      res.render('login', {
+        title: 'Login',
+        errors: errors.array()
+      })
     } else {
-      const { username, password } = req.body;
-      const user = await User.findOne({ username });
-      const isPasswordMatch = await user.matchPassword(password);
-      if (!user && !isPasswordMatch) {
-        return res.render("login", {
-          title: "Login",
+      const { username, password } = req.body
+      const user = await User.findOne({ username })
+      const isPasswordMatch = user ? await user.matchPassword(password) : false
+      if (!user || !isPasswordMatch) {
+        return res.render('login', {
+          title: 'Login',
           errors: [
             {
-              msg: "Invalid username or password",
-            },
-          ],
-        });
+              msg: 'Invalid username or password'
+            }
+          ]
+        })
       } else {
         const payload = {
           id: user._id,
           role: user.role,
           username: user.username,
-          avatar: user.avatar,
-        };
+          avatar: user.avatar
+        }
 
         const token = jwt.sign(payload, JWT_SECRET, {
-          expiresIn: "5 days",
-          algorithm: "HS256",
-        });
+          expiresIn: '5 days',
+          algorithm: 'HS256'
+        })
 
-        res.cookie("token", token, {
+        res.cookie('token', token, {
           maxAge: 5 * 24 * 60 * 60 * 1000,
-          secure: NODE_ENV === "production" ? true : false,
+          secure: NODE_ENV === 'production' ? true : false,
           httpOnly: true,
-          path: "/",
-        });
+          path: '/'
+        })
 
-        res.redirect("home");
+        res.redirect('home')
       }
     }
-  }),
-];
+  })
+]
 
 exports.logout_post = (req, res, next) => {
-  res.cookie("token", "");
-  res.redirect("home");
+  res.cookie('token', '')
+  res.redirect('home')
 }
--- controllers/exam.controllers.js
+++ controllers/exam.controllers.js
@@ -1,69 +1,83 @@
-const Exam = require("../models/exam");
-const asyncHandler = require("express-async-handler");
-const { body, validationResult } = require("express-validator");
-const getWrongAnswers = require("../Ai/getWrongAnswers");
+const Exam = require('../models/exam')
+const asyncHandler = require('express-async-handler')
+const { body, validationResult } = require('express-validator')
+const getWrongAnswers = require('../Ai/getWrongAnswers')
 
 // POST: create exam
 exports.exam_create = [
-  body("topic")
+  body('topic')
     .isLength({ min: 5 })
-    .withMessage("Topic must be more than 5 characters"),
-  body("questions_number")
-    .isLength({ min: 5 })
-    .withMessage("Number of questions must be more than 5 characters")
+    .withMessage('Topic must be more than 5 characters'),
+  body('questions_number')
     .isNumeric()
-    .withMessage("Number of questions must be a number"),
-  body("type")
-    .isIn(["mcq", "essay"])
+    .withMessage('Number of questions must be a number'),
+  body('type')
+    .isIn(['mcq', 'essay'])
     .withMessage("Type must be either 'mcq' or 'essay'"),
   asyncHandler(async (req, res, next) => {
-    const errors = validationResult(req);
+    const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      res.render("exam-form-create", {
-        title: "Exam Form",
-        errors: errors.array(),
-      });
+      res.render('home', {
+        title: 'Exam Form',
+        errors: errors.array()
+      })
     } else {
-      const { topic, type, questions_number } = req.body;
+      const { topic, type, questions_number } = req.body
       const exam = new Exam({
         topic,
         type,
         numberOfQuestions: questions_number,
-        teacher: req.user._id, // get from the cookie middlewared
-      });
+        teacher: req.user.id // get from the cookie middlewared
+      })
       try {
-        await exam.save();
-        res.redirect("/exams");
+        const newExam = await exam.save()
+        res.redirect(newExam.url);
       } catch (err) {
-        console.log(err);
+        console.log(err)
       }
     }
-  }),
-];
+  })
+]
 
 exports.exam_get = asyncHandler(async (req, res, next) => {
-  const exam = await Exam.findById(req.params.id);
-  res.render("exam", {
-    title: "Exam",
-    exam: exam,
-  });
-});
+  const exam = await Exam.findById(req.params.id).select('-students').exec();
+
+  if (!exam) {
+    const error = new Error('Exam not found')
+    error.status = 404
+    return next(error)
+  }
+  if (req.user.role === 'student') {
+    res.render('exam_form', {
+      title: 'exam form',
+      exam: exam
+    })
+  } else {
+    res.render('exam_view', {
+      title: 'Exam',
+      exam: exam
+    })
+  }
+})
 
 exports.exam_submit = asyncHandler(async (req, res, next) => {
   // we got the answers from the client
   // check the validity of the answers
   // and then return the exam with the correct answers
-  const exam = await Exam.findById(req.params.id);
-  const { answers } = req.body;
+  const exam = await Exam.findById(req.params.id)
+  const { answers } = req.body
   const correctAnswers =
-    exam.type === "mcq"
+    exam.type === 'mcq'
       ? exam.mcqQuestions.map((question) => question.answer)
-      : exam.essayQuestions.map((question) => question.answer);
+      : exam.essayQuestions.map((question) => question.answer)
   const wrongAnswers = getWrongAnswers(exam.type, correctAnswers, answers);
-  res.render("exam", {
-    title: "Exam Result",
+
+  // update the exam schmea with the student's score
+  
+  res.render('exam', {
+    title: 'Exam Result',
     wrongAnswers,
     exam,
-    score: (wrongAnswers.length / correctAnswers.length) * 100,
-  });
-});
+    score: (wrongAnswers.length / correctAnswers.length) * 100
+  })
+})
