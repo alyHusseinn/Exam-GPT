1b09df71de35e3c7e7cdaaa3092baa7e84f19bf4
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-05-22 01:16:02 +0300

    update the checkRole middleware to accept array of roles

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,16 +1,15 @@
 const jwt = require("jsonwebtoken");
 const asyncHanlder = require("express-async-handler");
 
-const checkRole = (role) => {
+const checkRole = (roles) => {
   return asyncHanlder((req, res, next) => {
     const token = req.cookies.token;
 
     if (!token) {
       return res.redirect("/login");
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded.role !== role) {
+    // const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!roles.includes(req.user.role)) {
       return res.status(403).send("Forbidden");
     }
     next();
