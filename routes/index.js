03c8c2114c17124949a92f75b461bf8ad262c09d
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-05-22 19:58:30 +0300

    adding the remove user and get all users ( students, teachers) routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const examControllers = require('../controllers/exam.controllers')
 const userControllers = require('../controllers/user.controllers')
 const authControllers = require('../controllers/auth.controllers')
-const oralExamControllers = require('../controllers/oralExam.controllers');
+const oralExamControllers = require('../controllers/oralExam.controllers')
 const checkJWT = require('../middlewares/checkJwt')
 const checkRole = require('../middlewares/checkRole')
 const router = require('express').Router()
@@ -9,42 +9,67 @@ const router = require('express').Router()
 router.get('/', function (req, res, next) {
   res.redirect('/home')
 })
+
 /* GET home page. */
 router.get('/home', checkJWT, userControllers.getHomePage)
 router.get('/teacher/:id', checkJWT, userControllers.getTeacher)
-router.get('/student/:id', checkJWT, userControllers.getStudent);
+router.get('/student/:id', checkJWT, userControllers.getStudent)
+router.get('/admin/:id', [checkJWT, checkRole(['admin'])], userControllers.getAdmin)
+
+router.post(
+  '/user/:id/delete',
+  [checkJWT, checkRole(['admin'])],
+  userControllers.deleteUser
+)
+router.get('/teachers', [checkJWT, checkRole(['admin'])], userControllers.getAllTeachers)
+router.get('/students', [checkJWT, checkRole(['admin'])], userControllers.getAllStudents)
 
 // Exam routes
-router.get('/exam/:id', checkJWT, examControllers.exam_get);
-router.get('/submition/:id', checkJWT, examControllers.exam_results_get);
-
-// post a new exam
+router.get('/exam/:id', checkJWT, examControllers.exam_get)
+router.get('/submition/:id', checkJWT, examControllers.exam_results_get)
+// post create a new exam
 router.post(
   '/exam',
-  [checkJWT, checkRole('teacher')],
+  [checkJWT, checkRole(['teacher'])],
   examControllers.exam_create
 )
 // submit an exam
 router.post(
   '/exam/:id',
-  [checkJWT, checkRole('student')],
+  [checkJWT, checkRole(['student'])],
   examControllers.exam_submit
 )
-
-router.post('/exam/:id/delete', [checkJWT, checkRole('teacher')], examControllers.exam_delete)
+router.post(
+  '/exam/:id/delete',
+  [checkJWT, checkRole(['teacher', 'admin'])],
+  examControllers.exam_delete
+)
 
 // Oral Exam routes
 // student submit the oral exam
-router.post('/oralexam/:id/submit', [checkJWT, checkRole('student')], oralExamControllers.oralExamSubmit);
-
+router.post(
+  '/oralexam/:id/submit',
+  [checkJWT, checkRole(['student'])],
+  oralExamControllers.oralExamSubmit
+)
 // get the oral exam correction form
-router.get('/oralexam/:id', [checkJWT, checkRole('teacher')], oralExamControllers.oralExamCorrection_form_get);
-
+router.get(
+  '/oralexam/:id',
+  [checkJWT, checkRole(['teacher'])],
+  oralExamControllers.oralExamCorrection_form_get
+)
 // get all the oral exams that needs to be corrected by the teacher
-router.get('/oralexams', [checkJWT, checkRole('teacher')], oralExamControllers.oralExamList);
-
+router.get(
+  '/oralexams',
+  [checkJWT, checkRole(['teacher'])],
+  oralExamControllers.oralExamList
+)
 // oral exam corrections form the teacher
-router.post('/oralexam/:id', [checkJWT, checkRole('teacher')], oralExamControllers.oralExamSubmitCorrection_post);
+router.post(
+  '/oralexam/:id',
+  [checkJWT, checkRole(['teacher'])],
+  oralExamControllers.oralExamSubmitCorrection_post
+)
 
 // Auth routesd
 router.get('/signup', authControllers.signup_get)
